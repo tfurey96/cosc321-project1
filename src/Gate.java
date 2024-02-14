d3ae193824851d16bbb4c4d5fd60a040f171411e
tfurey96 <98403865+tfurey96@users.noreply.github.com>
2024-02-14 16:24:50 -0500

    feat: modify gate gate now automaticlly runs the set output when both inputs are set

--- src/Gate.java
+++ src/Gate.java
@@ -1,6 +1,10 @@
 public class Gate {
-    int input1;
-    int input2;
+    /**
+     * Gate object that simulates a nor gate
+     * takes two binary input values and uses nor logic gate to create an output
+     */
+    int input1=-1;
+    int input2=-1;
 
     int output;
 
@@ -20,16 +24,30 @@ public class Gate {
         return input1;
     }
 
+    /***
+     * sets value of input1
+     * if input 2 is already set then it runs the logic gate and sets the output
+     * @param input1 value to be set as input1
+     */
     public void setInput1(int input1) {
         this.input1 = input1;
+        if (input2!=-1){
+            setOutput();
+        }
     }
 
     public int getInput2() {
         return input2;
     }
 
+    /***
+     * sets value of input2
+     * if input 1 is already set then it runs the logic gate and sets the output
+     * @param input2 value to be set as input2
+     */
     public void setInput2(int input2) {
         this.input2 = input2;
+        if (input1 != -1) setOutput();
     }
 
 
