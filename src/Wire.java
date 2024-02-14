25bd8c5940ddbf9f9c7fc45686805a4bc1916c05
tfurey96 <98403865+tfurey96@users.noreply.github.com>
2024-02-14 16:23:38 -0500

    Feat: create wire class created input and outputs for wire objects created getter method for input and output

---
+++ src/Wire.java
@@ -0,0 +1,17 @@
+public class Wire {
+    String input;
+    String output;
+
+    public Wire(String input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+}
