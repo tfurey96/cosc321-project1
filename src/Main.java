67c9dc057df62ddaadb480a71ea4d13d3b2269c4
tfurey96 <98403865+tfurey96@users.noreply.github.com>
2024-03-07 18:24:14 -0500

    read input file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
+    public static int numOfInputs=0;
+    public static int numOfGateCol=0;
+    public static int numOfgateRow=0;
 
+    public static void main(String[] args) {
+        File input = new File("input.txt");
+        ArrayList<String> wireList= readFile(input);
+
+
+    }
+
+    /**
+     * Function that sets the num of inputs and the size of the gate array
+     * and returns a list of wires
+     * @param input the input file
+     * @return
+     */
+    public static ArrayList<String> readFile(File input){
+        try {
+
+            Scanner fileReader = new Scanner(input);
+            numOfInputs=fileReader.nextInt();
+            numOfGateCol=fileReader.nextInt();
+            numOfgateRow=fileReader.nextInt();
+
+            ArrayList<String> wireList= new ArrayList<>();
+
+            while (fileReader.hasNextLine()){
+                wireList.add(fileReader.nextLine());
+            }
+            return wireList;
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
     }
 }
